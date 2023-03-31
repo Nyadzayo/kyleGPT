946ffb2fb58289b7e90788abd5aaafe2bc9e8892
GitHub Action <action@github.com>
2023-03-31 10:21:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/nyadzayo/kylegpt/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nyadzayo/kylegpt/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.nyadzayo.kylegpt.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nyadzayo.kylegpt.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
